ff542973223daeb1cf777f6c067c5fe04f3aafb6
OmarTaher99 <Omartaher_99@hotmail.com>
2021-01-23 13:51:06 +0300

    Minor Fixes

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.runtime.onMessage.addListener(function(msg, sender, response) {
     if (msg.from == "popup" && msg.to == "background" && msg.subject == "start-process") {
         // console.log(chrome.tabs.query({ active: true, currentWindow: true }))
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            // console.log(tabs)
             chrome.tabs.sendMessage(tabs[0].id, { from: "background", to: "content.js", subject: "sendDOM" });
         });
     };
